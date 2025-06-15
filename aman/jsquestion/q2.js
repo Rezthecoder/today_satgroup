e40af71adbc76ea48a81d583e31944f08b78eddc
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-06-15 10:26:42 +0900

    question 2 solve

--- aman/jsquestion/q2.js
+++ aman/jsquestion/q2.js
@@ -1,7 +1,12 @@
 // how to convert "123" into number
 const n="123"
 console.log(typeof n);
-// change into number
-const n1=n.toString()
+// change into number==> to use Number tar N capital latter hunu parxa
+const n1=Number(n)
 console.log(n1);
 console.log(typeof n1);
+
+// o rby using parstint method
+const a="1235"
+const b=parseInt(a)
+console.log(b +`_the type of b is `+typeof b);
