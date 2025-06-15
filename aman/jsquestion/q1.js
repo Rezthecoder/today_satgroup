fee3bbabb58556d8c11cc99feec5aae3452e20bf
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-06-15 10:14:19 +0900

    question 1 solve

---
+++ aman/jsquestion/q1.js
@@ -0,0 +1,2 @@
+console.log(typeof 42);
+// the type od 42 is number
---
+++ aman/jsquestion/q2.js
@@ -0,0 +1,7 @@
+// how to convert "123" into number
+const n="123"
+console.log(typeof n);
+// change into number
+const n1=n.toString()
+console.log(n1);
+console.log(typeof n1);
