4e7ef08d9d8806314e5f01d037232091a22abca4
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-06-16 20:25:54 +0900

    q5 and q4 will be solved

---
+++ aman/jsquestion/q4.js
@@ -0,0 +1,8 @@
+// which method return a numberwith 2 decimal places
+// let num=3.14159;
+// console.log(Number.___(2));
+
+let num=3.14159
+console.log(num.toFixed(2));
+// asko lagi toFixed(value)method use garinxa ra 
+// vaule ma decimal paxi baki rakna chaine value halinxa
---
+++ aman/jsquestion/q5.js
@@ -0,0 +1,5 @@
+// what does parseInt("10.5 ") return??
+let a=parseInt("10.5")
+console.log(a);
+console.log(typeof a);
+// parseint le decimal value sabai hataunxa ra value number ma dinxa
