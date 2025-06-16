bb202dde7d4712a732c0c387893b517d1fee5d95
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-06-16 10:01:34 +0900

    copy file

---
+++ aman/github.js
@@ -0,0 +1,15 @@
+//1> about this folder
+// mero branch  file name "amanbranch " ho
+
+// 2>branch ma change garna command
+/* git status
+git switch amanbranch */
+
+// 3>branch banaune command
+/* git branch filename */
+
+// 4> branch kati ota xa herne command
+/* git branch */
+
+// 5>main ma vako file same to same branch file ma copy garda
+ /* git reset --hard origin/main */
