787c6ae5a2a39c642237f93d8e90497059cae4fb
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-06-25 10:15:03 +0900

    solve29 and 30 question

--- aman/jsquesitonpart2/q29.js
+++ aman/jsquesitonpart2/q29.js
@@ -1,1 +1,7 @@
 // 29. How do you loop through an array?
+let arr=[1,2,3,5,6,"aman"]
+// for loop lako
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+
+}
---
+++ aman/jsquesitonpart2/q30.js
@@ -0,0 +1,6 @@
+// How do you merge two arrays?
+let a=[1,2,3,5]
+let b=[6,7,8,9]
+// to merge use concat method
+let c=a.concat(b)
+console.log(c);
