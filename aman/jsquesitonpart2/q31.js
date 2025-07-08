4b4266d64c5cc951dd3c31042a08f14094df6e9f
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-07-08 09:33:54 +0900

    31question solved

---
+++ aman/jsquesitonpart2/q31.js
@@ -0,0 +1,8 @@
+/* // 31. Destructure the first and second elements:
+    let arr = [10, 20];
+    let [a, b] = arr */
+    let arr=[10,20];
+    let [a,b]=arr;
+    // answer
+    console.log(a);
+    console.log(b);
