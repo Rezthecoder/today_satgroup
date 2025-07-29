c6d569df0dc9a36da3b6bed1ecda83052c8515f7
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2025-07-29 09:14:59 +0900

    q34andq35 be solved

---
+++ aman/jsquesitonpart2/q34.js
@@ -0,0 +1,8 @@
+// What does this do?
+//     const [first, ...rest] = [1, 2, 3, 4];
+//     console.log(rest)
+
+const [first,...rest]=[1,2,3,4]
+console.log(rest);
+
+// first value is distracted from array and new array be reated
---
+++ aman/jsquesitonpart2/q35.js
@@ -0,0 +1,5 @@
+//  What is the output of:
+//     const {a = 10} = {};
+let {a=10}={}
+console.log(a);
+// the value of a is 10
